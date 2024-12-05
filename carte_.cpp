0a5988deead8b220eacd34b0ae5e4891fc3318f1
Holy-Grm <oligermain15@gmail.com>
2024-12-05 10:17:28 -0500

    tableau d essaie case

--- carte_.cpp
+++ carte_.cpp
@@ -26,14 +26,11 @@ Compteur Carte::getNbMinesAdjacentes(Position e_pos)
 		for (int pos_colonne = 0;pos_colonne < 2;pos_colonne++)
 		{
 			Position verif_pos(pos_ligne, pos_colonne);
-			//Dans cette boucle verifier si la case est dans la carte
-			if (estDansCarte(verif_pos))
+			if (estDansCarte(verif_pos)) // verifie si case est valide
 			{
-				//Verfifier si les cases verifiers sont des mines ou des riens estUneMine()
-				if (m_cases[pos_ligne][pos_colonne].estUneMine())
+				if (m_cases[pos_ligne][pos_colonne].estUneMine()) // Verifie si les cases adjacentes sont des mines
 				{
-					//Incrementer compteur du nombre de mines adjacent si case = mine
-					++m_cases[pos_ligne][pos_colonne];
+					++m_cases[pos_ligne][pos_colonne]; // compteur de mine adjacente
 				}
 			}
 		}
@@ -47,133 +44,62 @@ Compteur Carte::getNbMinesAdjacentes(Position e_pos)
 // retour : booleen - vrai si ce n'est pas une mine, faux sinon
 // post : si la case a deja ete ouverte, ce n'est pas une mine -> retourne vrai, si la case n'est pas une mine, les cases voisines doivent etre ouvertes jusqu'a une prochaine mine
 
+// Fonction pour essayer d'ouvrir une case donnée et ses voisines si nécessaire
+// Fonction pour essayer d'ouvrir une case donnée et ses voisines si nécessaire
 bool Carte::essaieCase(Position e_pos)
 {
-	//Verifie si la case a deja ete ouverte
-	int ligne = e_pos.ligne();
-	int colonne = e_pos.colonne();
+    // Vérifier si la case est déjà ouverte
+    if (m_cases[e_pos.ligne()][e_pos.colonne()].estOuverte())
+    {
+        return true;
+    }
 
-	if (m_cases[ligne][colonne].estOuverte())
-	{
-		return true;
-	}
-	else
-	{
-		//Sinon verifie si la case est une mine
-		if (m_cases[ligne][colonne].estUneMine())
-		{
-			return false;
-		}
-		else
-		{
-			//Ouvrir les cases voisines jusqu'a une mine
+    // Si la case est une mine, retourner faux
+    if (m_cases[e_pos.ligne()][e_pos.colonne()].estUneMine())
+    {
+        return false;
+    }
 
-			//Ouvrir les cases vertical vers le bas
-			for (int i = ligne;i <= nbLignes();i++)
-			{
-				Position new_pos(ligne, colonne);
-				if (m_cases[ligne][colonne].estUneMine()) {}
-				else
-				{
-					ouvreCase(new_pos);
-				}
-			}
+    // Ouvrir la case courante
+    ouvreCase(e_pos);
 
-			//Ouvrir les cases vertical vers le haut
-			for (int i = ligne;i >= 0;i--)
-			{
-				Position new_pos(ligne, colonne);
-				if (m_cases[ligne][colonne].estUneMine()) {}
-				else
-				{
-					ouvreCase(new_pos);
-				}
-			}
+    // Déplacements possibles : (ligne, colonne)
+    const int directions[8][2] = {
+        {-1, -1}, // Haut Gauche
+        {-1, 0},  // Haut
+        {-1, 1},  // Haut Droite
+        {0, -1},  // Gauche
+        {0, 1},   // Droite
+        {1, -1},  // Bas Gauche
+        {1, 0},   // Bas
+        {1, 1}    // Bas Droite
+    };
 
-			//Ouvrir les cases horizontal vers la gauche
-			for (int i = colonne;i >= 0;i--)
-			{
-				Position new_pos(ligne, colonne);
-				if (m_cases[ligne][colonne].estUneMine()) {}
-				else
-				{
-					ouvreCase(new_pos);
-				}
-			}
+    // Parcourir chaque direction
+    for (int i = 0; i < 8; ++i)
+    {
+        int nouvelleLigne = e_pos.ligne() + directions[i][0];
+        int nouvelleColonne = e_pos.colonne() + directions[i][1];
 
-			//Ouvrir les cases horizontal vers la droite
-			for (int i = colonne;i <= nbColonnes();i++)
-			{
-				Position new_pos(ligne, colonne);
-				if (m_cases[ligne][colonne].estUneMine()) {}
-				else
-				{
-					ouvreCase(new_pos);
-				}
-			}
+        Position nouvellePos(nouvelleLigne, nouvelleColonne);
 
-			//Ouvrir les cases diagonal haut vers la gauche
-			for (int i = colonne;i >= 0;i--)
-			{
-				for (int e = ligne;e <= nbLignes();e++)
-				{
-					Position new_pos(ligne, colonne);
-					if (m_cases[ligne][colonne].estUneMine()) {}
-					else
-					{
-						ouvreCase(new_pos);
-					}
-				}
-			}
+        // Vérifier si la position est dans la carte
+        if (estDansCarte(nouvellePos) && !m_cases[nouvelleLigne][nouvelleColonne].estOuverte())
+        {
+            // Si la case n'est pas une mine, l'ouvrir
+            if (!m_cases[nouvelleLigne][nouvelleColonne].estUneMine())
+            {
+                ouvreCase(nouvellePos);
 
-			//Ouvrir les cases diagonal haut vers la droite
-			for (int i = colonne;i <= nbColonnes();i++)
-			{
-				for (int e = ligne;e <= nbLignes();e++)
-				{
-					Position new_pos(ligne, colonne);
-					if (m_cases[ligne][colonne].estUneMine()) {}
-					else
-					{
-						ouvreCase(new_pos);
-					}
-				}
-			}
+                // Appeler récursivement pour propager l'ouverture
+                essaieCase(nouvellePos);
+            }
+        }
+    }
 
-			//Ouvrir les cases diagonal bas vers la gauche
-			for (int i = colonne;i >= 0;i--)
-			{
-				for (int e = ligne;e >= 0;e--)
-				{
-					Position new_pos(ligne, colonne);
-					if (m_cases[ligne][colonne].estUneMine()) {}
-					else
-					{
-						ouvreCase(new_pos);
-					}
-				}
-			}
+    return true;
+}
 
-			//Ouvrir les cases diagonal bas vers la droite
-			for (int i = colonne;i >= 0;i--)
-			{
-				for (int e = ligne;e <= nbLignes();e++)
-				{
-					Position new_pos(ligne, colonne);
-					if (m_cases[ligne][colonne].estUneMine()) {}
-					else
-					{
-						ouvreCase(new_pos);
-					}
-				}
-			}
-		}
-	}
-
-	//Refaire completement recursivement
-
-	return true;
-};
 
 // Description: Methode qui calcule le nombre de mines adjacentes pour les cases libres de la carte
 // post : pour chacune des cases qui ne contient pas une mine, on compte le nombre de mines adjacentes
